c33e3226c5310a1aab5d898cc8d84a8a6302ab80
Smittyvb <smittyvb@gmail.com>
2017-11-04 08:04:39 -0400

    Add "approve", and "disapprove"

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ if (PROD) {
 }
 const THRESHOLD = 4;
 const { exec } = require('child_process');
-const UPVOTES = ["+1", "upvote", "i like it", "i really like it"];
-const DOWNVOTES = ["-1", "downvote", "i dislike it", "i really dislike it", "i hate it", "i really hate it"];
+const UPVOTES = ["+1", "upvote", "i like it", "i really like it", "approve"];
+const DOWNVOTES = ["-1", "downvote", "i dislike it", "i really dislike it", "i hate it", "i really hate it", "disapprove"];
 var GitHub = require('github-api');
 
 // basic auth
